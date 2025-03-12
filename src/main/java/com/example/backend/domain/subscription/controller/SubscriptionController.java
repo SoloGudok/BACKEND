6215de940d43303f904dc2ff8a267e1a2e5af1c8
k2xh <k2xh0115@gmail.com>
2025-03-12 16:35:24 +0900

    SubscriptionController 원상복귀

--- src/main/java/com/example/backend/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/example/backend/domain/subscription/controller/SubscriptionController.java
@@ -1,15 +1,17 @@
 package com.example.backend.domain.subscription.controller;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import com.example.backend.domain.subscription.dto.CombinationSubscriptionResponseDto;
 import com.example.backend.domain.subscription.dto.SubscriptionDTO;
 import com.example.backend.domain.subscription.dto.SubscriptionRes;
 import com.example.backend.domain.subscription.dto.SubscriptionResponseDto;
+import com.example.backend.domain.subscription.entity.Category;
+import com.example.backend.domain.subscription.entity.Subscription;
 import com.example.backend.domain.subscription.service.SubscriptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -80,4 +82,7 @@ public class SubscriptionController {
     public List<SubscriptionDTO> getAllSubscriptions() {
         return subscriptionService.getAllSubscriptions();
     }
+
+
+
 }
