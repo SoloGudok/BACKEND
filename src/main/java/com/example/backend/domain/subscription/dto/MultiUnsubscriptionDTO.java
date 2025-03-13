f904e306d85727161d145317889dd6b7a868f40a
Minkyuuu <145241803+Minkyuuu@users.noreply.github.com>
2025-03-13 19:23:28 +0900

    Create : 해지api 완료

--- src/main/java/com/example/backend/domain/subscription/dto/MultiUnsubscriptionDTO.java
+++ src/main/java/com/example/backend/domain/subscription/dto/MultiUnsubscriptionDTO.java
@@ -5,9 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MultiUnsubscriptionDTO {
+    private List<Long> subscriptionIds;
 }
