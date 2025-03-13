c95ea6af9f3f8f1b82a1d0d800d6cd55a94beb45
Minkyuuu <145241803+Minkyuuu@users.noreply.github.com>
2025-03-13 16:40:53 +0900

    Create : 단일 구독 해지 api

--- src/main/java/com/example/backend/domain/subscription/service/UnsubscriptionService.java
+++ src/main/java/com/example/backend/domain/subscription/service/UnsubscriptionService.java
@@ -1,5 +1,6 @@
 package com.example.backend.domain.subscription.service;
 
+import com.example.backend.domain.subscription.dto.UniUnsubscriptionDTO;
 import com.example.backend.domain.subscription.dto.UnsubscriptionDTO;
 import com.example.backend.domain.subscription.repository.UnsubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,19 @@ public class UnsubscriptionService {
     // 해지하려고 하는 서비스 이미지 추출 (민규)
     public UnsubscriptionDTO getUnsub(Long id) {
         return unsubscriptionRepository.getSubImg(id);
+    }
+
+    // 단일 해지 (민규)
+    public void processUnsubscription(UniUnsubscriptionDTO requestDTO) {
+        // 요청 데이터를 바탕으로 해지 처리
+        try {
+            unsubscriptionRepository.processUnsub1(
+                    requestDTO.getEmail(),
+                    requestDTO.getPassword(),
+                    requestDTO.getServiceId()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("해지 요청 중 오류 발생: " + e.getMessage());
+        }
     }
 }
