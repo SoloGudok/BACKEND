8012a8f589e69cf3e5d87dcb3392eea5c3f9bfa7
k2xh <k2xh0115@gmail.com>
2025-03-14 12:17:20 +0900

    #Fix : dashboard, recommend 컨트롤러에 @CrossOrigin 추가

--- src/main/java/com/example/backend/domain/user/controller/DashboardController.java
+++ src/main/java/com/example/backend/domain/user/controller/DashboardController.java
@@ -7,6 +7,7 @@ import com.example.backend.domain.user.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 @RequestMapping("/dashboard")
 @Log4j2
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class DashboardController {
     private final DashboardService dashboardService;
 
