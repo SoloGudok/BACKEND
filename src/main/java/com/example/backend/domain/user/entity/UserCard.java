17a4214b7bc7754be5adf0f3d4dd98aff26bb7da
minjeong <allovella7@gmail.com>
2025-03-17 17:27:28 +0900

    Fix: 구독결제 관련 클래스 수정

--- src/main/java/com/example/backend/domain/user/entity/UserCard.java
+++ src/main/java/com/example/backend/domain/user/entity/UserCard.java
@@ -1,4 +1,19 @@
 package com.example.backend.domain.user.entity;
 
+import com.example.backend.domain.card.entity.Card;
+import jakarta.persistence.*;
+
+@Entity
 public class UserCard {
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;  // Card와의 관계 설정
 }
+
